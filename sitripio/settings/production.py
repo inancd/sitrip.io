73a7b66cc41491823ce13c5fe5acc08ed2fc2a18
inanc dagyolu <inancdagyolu@inancs-Air.home>
2023-03-18 17:09:41 +0300

    Database settings

--- sitripio/settings/production.py
+++ sitripio/settings/production.py
@@ -6,11 +6,11 @@ STATIC_ROOT = "/home/innolove/sitrip.io/static"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '',
+        'PORT': config('DB_PORT'),
     }
 }
